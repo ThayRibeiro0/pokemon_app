c1c2d604c2dea26b4f958b85b6326426d84ac9be
AdrewReyes <andrewreyes.m@gmail.com>
2025-01-23 18:57:27 -0800

    favorites.js update to link to detail.html

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,54 +1,42 @@
-// https://www.w3schools.com/jsref/met_document_queryselector.asp
-const listWrapper = document.querySelector(".list-wrapper");
-const notFoundMessage = document.querySelector("#not-found-message");
-
-// Load favorites from local
-const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-
-// Function to display only favorited Pokémon
+// Function to display favorites
 function displayFavorites() {
-  listWrapper.innerHTML = ""; // Clear the current content
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const listWrapper = document.querySelector(".list-wrapper");
 
-  if (favorites.length === 0) {
-    // Show "Choose your pokemon" message if nothing is selected
-    notFoundMessage.style.display = "block";
-    return;
-  }
+  listWrapper.innerHTML = "";
 
-  // Hide "No favorites added yet!" message
-  notFoundMessage.style.display = "none";
-
-// took from pokemon.js for display
-  // list of favorited Pokémon and display them
   favorites.forEach((pokemon) => {
     const listItem = document.createElement("div");
     listItem.className = "list-item";
     listItem.innerHTML = `
-        <div class="number-wrap">
-            <p class="caption-fonts">#${pokemon.id}</p>
-        </div>
-        <div class="img-wrap">
-            <img src="${pokemon.img}" alt="${pokemon.name}" />
-        </div>
-        <div class="name-wrap">
-            <p class="body3-fonts">${pokemon.name}</p>
-        </div>
-        <button class="remove-btn" data-id="${pokemon.id}">Remove</button>
+      <div class="number-wrap">
+          <p class="caption-fonts">#${pokemon.id}</p>
+      </div>
+      <div class="img-wrap">
+          <img src="${pokemon.img}" alt="${pokemon.name}" />
+      </div>
+      <div class="name-wrap">
+          <p class="body3-fonts">${pokemon.name}</p>
+      </div>
+      <button class="remove-btn">Remove</button>
     `;
-// remove button?
-    // Add a click event listener to the "Remove" button
+
     const removeButton = listItem.querySelector(".remove-btn");
     removeButton.addEventListener("click", () => {
       removeFromFavorites(pokemon.id); // Call the remove function
+    });
+
+    listItem.addEventListener("click", () => {
+      window.location.href = `./detail.html?id=${pokemon.id}`; // Redirect to detail.html
     });
 
     listWrapper.appendChild(listItem); // Append the item to the list
   });
 }
 
-//i need a function to remove if i do not want the pokemon 
 // Function to remove a Pokémon from the favorites list
 function removeFromFavorites(id) {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   // Filter out the Pokémon to be removed
   const updatedFavorites = favorites.filter((pokemon) => pokemon.id !== id);
   localStorage.setItem("favorites", JSON.stringify(updatedFavorites)); // Update localStorage
